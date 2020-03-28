c854337de29ce2b0c370e54e8b738cf1858de00a
vitthal-inani <vitthal.inani@gmail.com>
2020-03-28 09:57:00 +0530

    Android app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:community_app/profile.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:community_app/Login.dart';
 import 'package:community_app/homepage.dart';
+import 'profile.dart';
 
 void main() {
   runApp(
@@ -17,7 +19,7 @@ class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: homePage(),
+      home: profile(),
     );
   }
 }
